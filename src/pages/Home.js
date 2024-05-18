2e20880c7b07aa0e29bdc3e3c0bed7c1f59266e7
Abraham-1994 <156835431+Abraham-1994@users.noreply.github.com>
2024-05-18 04:20:21 +0300

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../Component/Header/Header";
+// import Footer from "../Component/Footer/Footer";
+import Banner from "../Component/Banner/Banner";
+import RowList from "../Rows/RowList/RowList";
+import Footer from "../Component/Footer/Footer"
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
